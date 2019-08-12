4bff977646eb37cd45ee3775a61601af3ad4fc57
Kevin Honnoll <khonnoll@gmail.com>
2019-08-12 01:52:12 -0400

    front end console logs object from api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    this.getStudents()
+  }
+
+  getStudents () {
+    fetch('/users')
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.error(error))
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Stephen Gilbert Musical Instruction</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
