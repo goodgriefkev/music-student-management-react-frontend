a32381917f18a80cbd825cc2dfe5a965f888d474
Kevin Honnoll <khonnoll@gmail.com>
2019-08-14 20:50:29 -0400

    Updates

---
+++ precache-manifest.ccd1609658177c9a12f1b4226730f9dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed299100844f9fa330fb17b680b2975",
+    "url": "/music-student-management-react-frontend/index.html"
+  },
+  {
+    "revision": "42090c9279ce32cdc11c",
+    "url": "/music-student-management-react-frontend/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f902c91f96982cdcf76e",
+    "url": "/music-student-management-react-frontend/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "42090c9279ce32cdc11c",
+    "url": "/music-student-management-react-frontend/static/js/2.f24bdffd.chunk.js"
+  },
+  {
+    "revision": "f902c91f96982cdcf76e",
+    "url": "/music-student-management-react-frontend/static/js/main.0ffb3398.chunk.js"
+  },
+  {
+    "revision": "1602be28b8f8a4936be8",
+    "url": "/music-student-management-react-frontend/static/js/runtime~main.224f4850.js"
+  }
+]);
